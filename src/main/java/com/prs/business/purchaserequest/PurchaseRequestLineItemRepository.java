a367ac17b1175923c523a7989514460cf00a7877
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-10-31 22:27:17 -0400

    PRS: Spring update to PRLI Repository

--- src/main/java/com/prs/business/purchaserequest/PurchaseRequestLineItemRepository.java
+++ src/main/java/com/prs/business/purchaserequest/PurchaseRequestLineItemRepository.java
@@ -2,11 +2,11 @@ package com.prs.business.purchaserequest;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prs.business.purchaserequest.PurchaseRequest;
 
-public interface PurchaseRequestLineItemRepository extends CrudRepository<PurchaseRequestLineItem, Integer> {
-	List<PurchaseRequestLineItem> findAllByPurchaseRequestId(int id);
+public interface PurchaseRequestLineItemRepository extends JpaRepository<PurchaseRequestLineItem, Integer> {
+	List<PurchaseRequestLineItem> findAllByPurchaseRequestId(int purchaseRequestId);
 
 }
